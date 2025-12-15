a8f7b37b39297ecb6d575f3f6829d2d281d7050c
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-15 19:56:21 +0530

    10-sg

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -8,3 +8,24 @@ module "frontend" {
   sg_description = var.frontend_sg_description
   vpc_id         = local.vpc_id
 }
+
+
+module "bastion" {
+  #source = "../../terraform-aws-securitygroup"
+  source      = "git::https://github.com/nagendrakeerthipati/terraform-aws-securitygroup.git?ref=main"
+  project     = var.project
+  environment = var.environment
+
+  sg_name        = var.bastion_sg_name
+  sg_description = var.bastion_sg_description
+  vpc_id         = local.vpc_id
+}
+
+resource "aws_vpc_security_group_rule" "bastion_ingress" {
+  security_group_id = module.bastion.sg_id
+  type              = "ingress"
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
