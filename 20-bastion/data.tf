14d26e3b33bfda8f587c0b77474c27f76bcc5e07
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2026-01-17 17:36:59 +0530

    Added bastion and backend ALB terraform modules

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,10 +1,11 @@
 data "aws_ami" "joindevops" {
-  owners      = ["973714476881"]
+  owners      = ["973714476881"] # JoinDevOps Account ID
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["RHEL-9-DevOps-Practice"]
+    values = ["Redhat-9-DevOps-Practice"]
+
   }
 
   filter {
